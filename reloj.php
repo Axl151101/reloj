7c531c9a56674604384ce7ba95fdc2861acec7d8
Axl151101 <84532278+Axl151101@users.noreply.github.com>
2023-02-22 23:12:57 -0500

    Add files via upload

---
+++ reloj.php
@@ -0,0 +1,14 @@
+<?php 
+
+date_default_timezone_set('America/Lima');
+
+$tiempo_en_segundos = time();
+$fecha_actual = date("d-m-Y h:i:s", $tiempo_en_segundos);
+echo "Autor: AXL WILLIAMS MEDRANO SOTO";
+echo "<br>";
+echo "<br>";
+echo "La fecha actual es: $fecha_actual";
+
+
+
+?>

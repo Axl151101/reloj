e987712b437bf26aac70b95ffd893650fd3ef59a
Axl151101 <84532278+Axl151101@users.noreply.github.com>
2023-02-23 00:48:09 -0500

    Update reloj.php

--- reloj.php
+++ reloj.php
@@ -1,14 +1,20 @@
 <form method="post" action="">
-    <label for="nombre">Ingrese su nombre:</label>
+    <label for="nombre">Ingrese un nombre:</label>
     <input type="text" name="nombre" id="nombre">
     <button type="submit">Guardar</button>
 </form>
 <?php 
-date_default_timezone_set('America/Lima');
-$tiempo_en_segundos = time();
-$fecha_actual = date("d-m-Y h:i:s", $tiempo_en_segundos);
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $nombre = $_POST['nombre'];
-    echo "Hola, $nombre!, la fecha y hora actual es: $fecha_actual";
+    file_put_contents("nombres.txt", $nombre . PHP_EOL, FILE_APPEND);
 }
+$nombres = file_get_contents("nombres.txt");
+$nombres_array = explode(PHP_EOL, $nombres);
+echo "<ul>";
+foreach ($nombres_array as $nombre) {
+    if (!empty($nombre)) {
+        echo "<li>$nombre</li>";
+    }
+}
+echo "</ul>";
 ?>

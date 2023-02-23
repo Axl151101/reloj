c6cbb3005e5d0508044039b3cfd6703daf479c1e
Axl151101 <84532278+Axl151101@users.noreply.github.com>
2023-02-23 00:25:49 -0500

    Update reloj.php

--- reloj.php
+++ reloj.php
@@ -1,14 +1,14 @@
+<form method="post" action="">
+    <label for="nombre">Ingrese su nombre:</label>
+    <input type="text" name="nombre" id="nombre">
+    <button type="submit">Guardar</button>
+</form>
 <?php 
-
 date_default_timezone_set('America/Lima');
-
 $tiempo_en_segundos = time();
 $fecha_actual = date("d-m-Y h:i:s", $tiempo_en_segundos);
-echo "Autor: AXL WILLIAMS MEDRANO SOTO";
-echo "<br>";
-echo "<br>";
-echo "La fecha actual es: $fecha_actual";
-
-
-
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $nombre = $_POST['nombre'];
+    echo "Hola, $nombre!, la fecha y hora actual es: $fecha_actual";
+}
 ?>

0b3aee6db41daf358c6495a4d771001da06e92f3
Axl151101 <84532278+Axl151101@users.noreply.github.com>
2023-02-23 12:16:56 -0500

    Update reloj.php

--- reloj.php
+++ reloj.php
@@ -4,10 +4,14 @@
     <button type="submit">Guardar</button>
 </form>
 <?php 
+date_default_timezone_set('America/Lima'); // establecer la zona horaria de Perú
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $nombre = $_POST['nombre'];
     file_put_contents("nombres.txt", $nombre . PHP_EOL, FILE_APPEND);
 }
+$hora_actual = date('H:i:s');
+echo '<h1> La hora actual en Perú es : ' . $hora_actual . '</h1>' ;
 $nombres = file_get_contents("nombres.txt");
 $nombres_array = explode(PHP_EOL, $nombres);
 echo "<ul>";
